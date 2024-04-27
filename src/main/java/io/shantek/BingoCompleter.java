82d812ec44798b79895e26ab8a96a026418ae85b
Shantek <kris@shantek.io>
2024-04-27 20:13:35 +0930

    Added speed run mode

--- src/main/java/io/shantek/BingoCompleter.java
+++ src/main/java/io/shantek/BingoCompleter.java
@@ -1,11 +1,11 @@
 package io.shantek;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,38 +15,32 @@ public class BingoCompleter implements TabCompleter {
         if (args.length == 1 && commandSender instanceof Player player) {
             List<String> complete = new ArrayList<>();
             if (player.hasPermission("shantek.ultimatebingo.start")) {
-                complete.add("gui");
+                complete.add("start");
             }
             if (player.hasPermission("shantek.ultimatebingo.stop")) {
                 complete.add("stop");
             }
             if (player.hasPermission("shantek.ultimatebingo.settings")) {
                 complete.add("settings");
-                complete.add("reload");
             }
 
-            complete.add("info");
-            complete.add("card");
             return StringUtil.copyPartialMatches(args[0], complete, new ArrayList<>());
-
-
-        } else if (args.length == 2 && args[0].equalsIgnoreCase("card")) {
-
-            List<String> playerNames = new ArrayList<>();
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                playerNames.add(player.getName());
-            }
-            return StringUtil.copyPartialMatches(args[1], playerNames, new ArrayList<>());
-        }
-    else if (args.length == 2 && args[0].equalsIgnoreCase("gamemode") && commandSender.hasPermission("shantek.ultimatebingo.configure")) {
-
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("start")) {
             List<String> complete = new ArrayList<>();
-            complete.add("traditional");
-            complete.add("reveal");
+            complete.add("same");
+            complete.add("unique");
 
             return StringUtil.copyPartialMatches(args[1], complete, new ArrayList<>());
-    }
+        } else if (args.length == 3 && args[0].equalsIgnoreCase("start") && (args[1].equalsIgnoreCase("same") || args[1].equalsIgnoreCase("unique"))) {
+            List<String> complete = new ArrayList<>();
+            complete.add("small");
+            complete.add("medium");
+            complete.add("large");
+
+            return StringUtil.copyPartialMatches(args[2], complete, new ArrayList<>());
+        }
 
         return null;
     }
+
 }
