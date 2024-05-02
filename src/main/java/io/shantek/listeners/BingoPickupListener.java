517cd1ea611b7499337f84b209f37786d5f0a706
Kris <kris@shantek.com.au>
2024-05-02 17:08:25 +0930

    Replaced deprecated PlayerPickup event with EntityPickup

--- src/main/java/io/shantek/listeners/BingoPickupListener.java
+++ src/main/java/io/shantek/listeners/BingoPickupListener.java
@@ -7,10 +7,9 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-
 import java.util.*;
 
 public class BingoPickupListener implements Listener {
@@ -20,40 +19,36 @@ public class BingoPickupListener implements Listener {
     }
 
     @EventHandler
-    public void onPickup (PlayerPickupItemEvent e){
-        if (ultimateBingo.getBingoManager().isStarted()){
-            BingoManager bingoManager = ultimateBingo.getBingoManager();
-            Material pickedItem = e.getItem().getItemStack().getType();
-            Player player = (Player) e.getPlayer();
+    public void onPickup(EntityPickupItemEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if (ultimateBingo.getBingoManager().isStarted()) {
+                BingoManager bingoManager = ultimateBingo.getBingoManager();
+                Material pickedItem = event.getItem().getItemStack().getType();
 
+                UUID uuid = player.getUniqueId();
+                Map<UUID, Inventory> bingoGUIs = bingoManager.getBingoGUIs();
+                MaterialList materialListObject = ultimateBingo.getMaterialList();
 
-            UUID uuid = player.getUniqueId();
+                List<Material> allMaterials = new ArrayList<>();
+                allMaterials.addAll(materialListObject.easy);
+                allMaterials.addAll(materialListObject.normal);
+                allMaterials.addAll(materialListObject.hard);
+                allMaterials.addAll(materialListObject.extreme);
+                allMaterials.addAll(materialListObject.impossible);
 
-            Map<UUID, Inventory> bingoGUIs = bingoManager.getBingoGUIs();
-
-            MaterialList materialListObject = ultimateBingo.getMaterialList();
-
-            List<Material> allMaterials = new ArrayList<>();
-            allMaterials.addAll(materialListObject.easy);
-            allMaterials.addAll(materialListObject.normal);
-            allMaterials.addAll(materialListObject.hard);
-            allMaterials.addAll(materialListObject.extreme);
-            allMaterials.addAll(materialListObject.impossible);
-
-            if (allMaterials.contains(pickedItem)){
-                for (int i : bingoManager.getSlots()) {
+                if (allMaterials.contains(pickedItem)) {
                     Inventory bingoGUI = bingoGUIs.get(uuid);
                     if (bingoGUI != null) {
-                        ItemStack bingoItem = bingoGUI.getItem(i);
-                        if (bingoItem != null && bingoItem.getType() == pickedItem) {
-                            bingoManager.markItemAsComplete(player, pickedItem);
+                        for (int i : bingoManager.getSlots()) {
+                            ItemStack bingoItem = bingoGUI.getItem(i);
+                            if (bingoItem != null && bingoItem.getType() == pickedItem) {
+                                bingoManager.markItemAsComplete(player, pickedItem);
+                            }
                         }
                     }
                 }
             }
-
         }
-
-
     }
 }
