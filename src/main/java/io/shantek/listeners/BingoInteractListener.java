d947f78476feab6f4e7f56e16e4cb984db90d910
Shantek <kris@shantek.io>
2024-05-02 11:15:17 +0930

    Tweaks to countdown and interacting with the bingo command

--- src/main/java/io/shantek/listeners/BingoInteractListener.java
+++ src/main/java/io/shantek/listeners/BingoInteractListener.java
@@ -1,6 +1,7 @@
 package io.shantek.listeners;
 
 import io.shantek.UltimateBingo;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -23,7 +24,14 @@ public class BingoInteractListener implements Listener {
         // Check if the player is holding the "Bingo" stick
         if (itemInHand != null && itemInHand.getType() == Material.PAPER && "Bingo".equals(itemInHand.getItemMeta().getDisplayName())) {
             // Run the "/bingo" command for the player
-            player.performCommand("bingo");
+
+            // If this is false, the bingo countdown is still running or they have an old card
+            if (ultimateBingo.bingoCardActive) {
+                player.performCommand("bingo");
+            } else {
+                player.sendMessage(ChatColor.RED + "Bingo hasn't started yet!");
+
+            }
 
             // Optionally, cancel the event to prevent the stick from being used as a normal stick
             event.setCancelled(true);
