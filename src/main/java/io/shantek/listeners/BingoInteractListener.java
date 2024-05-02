83760edd9512b4f63b0b834e265d52533fd034dd
Kris <kris@shantek.com.au>
2024-05-02 18:09:45 +0930

    Changed name of Bingo card in hand

--- src/main/java/io/shantek/listeners/BingoInteractListener.java
+++ src/main/java/io/shantek/listeners/BingoInteractListener.java
@@ -22,7 +22,7 @@ public class BingoInteractListener implements Listener {
         ItemStack itemInHand = event.getItem();
 
         // Check if the player is holding the "Bingo" stick
-        if (itemInHand != null && itemInHand.getType() == Material.PAPER && "Bingo".equals(itemInHand.getItemMeta().getDisplayName())) {
+        if (itemInHand != null && itemInHand.getType() == Material.PAPER && itemInHand.getItemMeta().getDisplayName().equals(ChatColor.GOLD + "Bingo Card")) {
 
             // Run the "/bingo" command for the player
             // If this is false, the bingo countdown is still running or they have an old card
