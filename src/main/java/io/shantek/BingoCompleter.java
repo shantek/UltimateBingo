633082507173373fb46e8675ad041001bf672927
Kris <kris@shantek.com.au>
2024-05-03 14:36:01 +0930

    Added game mode functionality with a new reveal game mode

--- src/main/java/io/shantek/BingoCompleter.java
+++ src/main/java/io/shantek/BingoCompleter.java
@@ -1,5 +1,6 @@
 package io.shantek;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -28,8 +29,10 @@ public class BingoCompleter implements TabCompleter {
                 complete.add("condition");
                 complete.add("difficulty");
                 complete.add("cardtype");
+                complete.add("gamemode");
             }
             complete.add("info");
+            complete.add("reveal");
             return StringUtil.copyPartialMatches(args[0], complete, new ArrayList<>());
         } else if (args.length == 2 && args[0].equalsIgnoreCase("cardtype") && commandSender.hasPermission("shantek.ultimatebingo.configure")) {
             List<String> complete = new ArrayList<>();
@@ -61,7 +64,22 @@ public class BingoCompleter implements TabCompleter {
 
             return StringUtil.copyPartialMatches(args[1], complete, new ArrayList<>());
 
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("reveal")) {
+
+            List<String> playerNames = new ArrayList<>();
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                playerNames.add(player.getName());
+            }
+            return StringUtil.copyPartialMatches(args[1], playerNames, new ArrayList<>());
         }
+    else if (args.length == 2 && args[0].equalsIgnoreCase("gamemode") && commandSender.hasPermission("shantek.ultimatebingo.configure")) {
+
+            List<String> complete = new ArrayList<>();
+            complete.add("traditional");
+            complete.add("reveal");
+
+            return StringUtil.copyPartialMatches(args[1], complete, new ArrayList<>());
+    }
 
         return null;
     }
