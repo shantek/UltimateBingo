26ea2146038b66c3ded34cf4f3b20a0c3106fcf3
Kris <kris@shantek.com.au>
2024-05-05 15:10:48 +0930

    Added player gui for /bingo command

---
+++ src/main/java/io/shantek/listeners/BingoPlayerGUIListener.java
@@ -0,0 +1,139 @@
+package io.shantek.listeners;
+
+import io.shantek.UltimateBingo;
+import io.shantek.managers.BingoPlayerGUIManager;
+import io.shantek.managers.SettingsManager;
+import io.shantek.tools.ItemBuilder;
+import io.shantek.tools.MaterialList;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+
+public class BingoPlayerGUIListener implements Listener {
+    MaterialList materialList;
+    public BingoPlayerGUIManager bingoPlayerGUIManager;
+    public UltimateBingo ultimateBingo;
+    private boolean sentWarning;
+
+    public BingoPlayerGUIListener(MaterialList materialList, BingoPlayerGUIManager bingoPlayerGUIManager, UltimateBingo ultimateBingo) {
+        this.materialList = materialList;
+        this.bingoPlayerGUIManager = bingoPlayerGUIManager;
+        this.ultimateBingo = ultimateBingo;
+        sentWarning = false;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+
+        if (!(e.getWhoClicked() instanceof Player)) return;
+
+        Player player = (Player) e.getWhoClicked();
+
+        // Ensure the event was triggered in the Bingo configuration GUI
+        if (e.getView().getTitle().equals(ChatColor.GOLD.toString() + ChatColor.BOLD + "Welcome to Ultimate Bingo")) {
+            e.setCancelled(true);  // Prevent dragging items
+
+            int slot = e.getRawSlot();
+            // Ensure clicks are within the inventory size
+            if (slot >= 0 && slot < 9) {
+                switch (slot) {
+                    case 0:
+                        ultimateBingo.bingoFunctions.giveBingoCard(player);
+                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0F, 1.0F);
+                        break;
+
+                    case 1:
+                        player.openInventory(ultimateBingo.bingoPlayerGUIManager.setupPlayersBingoCardsInventory());
+                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0F, 1.0F);
+                        break;
+                        /*
+                    case 2:
+                        bingoGameGUIManager.toggleCardSize(player);
+                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0F, 1.0F);
+                        break;
+                    case 3:
+                        bingoGameGUIManager.toggleCardType(player);
+                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0F, 1.0F);
+                        break;
+                    case 4:
+                        bingoGameGUIManager.toggleWinCondition(player);
+                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0F, 1.0F);
+                        break;
+                    case 5:
+                        bingoGameGUIManager.toggleRevealCards(player);
+                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0F, 1.0F);
+                        break;
+                    case 8:
+                        ultimateBingo.bingoCommand.startBingo(player);
+                        ultimateBingo.bingoSpawnLocation = player.getLocation();
+                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0F, 1.0F);
+                        player.closeInventory();
+                        break;
+
+                         */
+                    default:
+                        // This case handles any undefined slots, no action is taken
+                        break;
+                }
+            }
+
+        } else if (e.getView().getTitle().contains("Player Bingo Cards")) {
+            e.setCancelled(true);  // Prevent dragging items
+
+            // Deal with the back to menu
+            int slot = e.getRawSlot();
+
+            if (slot == 53) {
+                ItemStack clickedItem = e.getCurrentItem();
+                if (clickedItem != null && clickedItem.getType() == Material.CHEST) {
+                    // Check the display name of the item to confirm it's the "Back to Menu" chest
+                    if (clickedItem.hasItemMeta() && clickedItem.getItemMeta().hasDisplayName()
+                            && ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName()).equals("Back to menu")) {
+
+                        player.closeInventory();
+                        player.openInventory(ultimateBingo.bingoPlayerGUIManager.createPlayerGUI(player));
+                    }
+                }
+            } else {
+
+                // Ensure the event was triggered in the list of player bingo cards
+                ItemStack clickedItem = e.getCurrentItem();
+                if (clickedItem == null || clickedItem.getType() != Material.FILLED_MAP) {
+                    return; // Not a valid item, ignore the click
+                }
+
+                // Get the item's display name and strip color codes to retrieve the player's name
+                String displayName = ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName());
+
+                // Check if a player with this name exists and is online
+                Player targetPlayer = Bukkit.getPlayerExact(displayName);
+                if (targetPlayer == null) {
+                    e.getWhoClicked().sendMessage(ChatColor.RED + "The player's bingo card you are trying to access does not exist or they are not online.");
+                    return; // No such player found or not online, ignore the click
+                }
+
+                // Close the current inventory
+                e.getWhoClicked().closeInventory();
+
+                // Call a method to open the bingo card of the target player
+                ultimateBingo.bingoCommand.openBingoOtherPlayer(player, targetPlayer);
+
+            }
+        }
+
+    }
+}
+
+
+
+
